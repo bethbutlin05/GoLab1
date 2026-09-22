e8e14292f94b80fd7bd9c6a3f9ff341aac266992
Bethany Butlin <bethanybutlin2005@gmail.com>
2026-09-22 12:46:30 +0100

    Completed question 3a & 3b

--- sequences/sequences.go
+++ sequences/sequences.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func addOne(a int) int {
 	return a + 1
 }
@@ -13,13 +15,29 @@ func double(slice []int) {
 }
 
 func mapSlice(f func(a int) int, slice []int) {
-
+	for i, value := range slice {
+		slice[i] = f(value)
+	}
 }
 
 func mapArray(f func(a int) int, array [3]int) {
+	for i, value := range array {
+		array[i] = f(value)
+		//changes only the copied array. the original ints in main stay unchanged
+		//this is different to the slice version, because slices refer to underlying data
+	}
 
 }
 
 func main() {
+	intsSlice := []int{1, 2, 3}
+	mapSlice(addOne, intsSlice)
+	fmt.Println(intsSlice)
 
+	intsArray := [3]int{1, 2, 3}
+	mapArray(addOne, intsArray)
+	fmt.Println(intsArray)
+
+	//arrays are copied when passed to a function and have a fixed length
+	//slices refer to the underlying elements and have variable lengths
 }
