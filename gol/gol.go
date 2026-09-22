aac1394d10ad9714d78767dd86422abba26a8409
Bethany Butlin <bethanybutlin2005@gmail.com>
2026-09-22 14:14:07 +0100

    Completed questions 4 & 5

--- gol/gol.go
+++ gol/gol.go
@@ -1,9 +1,56 @@
 package main
 
 func calculateNextState(p golParams, world [][]byte) [][]byte {
-	return world
+	newWorld := make([][]byte, p.imageHeight)
+	for y := 0; y < p.imageHeight; y++ {
+		newWorld[y] = make([]byte, p.imageWidth)
+	}
+	for y := 0; y < p.imageHeight; y++ {
+		for x := 0; x < p.imageWidth; x++ {
+			aliveNeighbours := 0
+			for dy := -1; dy <= 1; dy++ {
+				for dx := -1; dx <= 1; dx++ {
+
+					//Don't count the cell itself
+					if dx == 0 && dy == 0 {
+						continue
+					}
+
+					neighbourX := (x + dx + p.imageWidth) % p.imageWidth
+					neighbourY := (y + dy + p.imageHeight) % p.imageHeight
+
+					if world[neighbourY][neighbourX] == 255 {
+						aliveNeighbours++
+					}
+				}
+			}
+
+			if world[y][x] == 255 {
+				if aliveNeighbours == 2 || aliveNeighbours == 3 {
+					newWorld[y][x] = 255
+				} else {
+					newWorld[y][x] = 0
+				}
+			} else {
+				if aliveNeighbours == 3 {
+					newWorld[y][x] = 255
+				} else {
+					newWorld[y][x] = 0
+				}
+			}
+		}
+	}
+	return newWorld
 }
 
 func calculateAliveCells(p golParams, world [][]byte) []cell {
-	return []cell{}
+	aliveCells := []cell{}
+	for y := 0; y < p.imageHeight; y++ {
+		for x := 0; x < p.imageWidth; x++ {
+			if world[y][x] == 255 {
+				aliveCells = append(aliveCells, cell{x: x, y: y})
+			}
+		}
+	}
+	return aliveCells
 }
--- hello.go
+++ hello.go
@@ -1,9 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	for i := 0; i < 20; i++ {
-		fmt.Println("Hello World")
+	for i := 0; i < 5; i++ {
+		go func(n int) {
+			fmt.Printf("Hello from goroutine %d\n", n)
+		}(i)
 	}
+	time.Sleep(1 * time.Second)
 }
